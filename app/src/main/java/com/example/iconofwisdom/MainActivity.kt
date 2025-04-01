4dba55ccbf8cf964da9b339737d9bea95017bcc0
jupiter1079 <ST10465627@rcconnect.edu.za>
2025-04-01 15:53:26 +0200

    changes

--- app/src/main/java/com/example/iconofwisdom/MainActivity.kt
+++ app/src/main/java/com/example/iconofwisdom/MainActivity.kt
@@ -10,11 +10,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -22,10 +19,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.iconofwisdom.ui.theme.IconOfWisdomTheme
 
 
 class MainActivity : ComponentActivity() {
@@ -59,7 +54,7 @@ class MainActivity : ComponentActivity() {
                        icon_fname = text
                    },
                    placeholder = {
-                       Text(text = "Enter Time of the day")
+                       Text(text = "Enter Time of the day.")
                    }
                )
                Spacer(modifier = Modifier.size(30.dp))
